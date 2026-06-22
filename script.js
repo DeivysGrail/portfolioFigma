ac75aaef7fc975b0a8dd38f2af99b61dd68b4832
DeivysGrail <66143151+DeivysGrail@users.noreply.github.com>
2026-06-22 08:48:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const linkDiv = document.getElementById("divLink")
+
+addEventListener((e) => {
+
+window.location("https://www.figma.com/proto/VGIeXCOyGLEANv5VcUZvGO/Portfolio-UX?page-id=0%3A1&node-id=98-264&viewport=-553%2C933%2C0.19&t=lxrL55vhUaQDs5h")
+  
+})
