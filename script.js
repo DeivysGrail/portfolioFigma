8339dc6cb20be7fd5b8a4b502452b1d55a07345f
DeivysGrail <66143151+DeivysGrail@users.noreply.github.com>
2026-06-22 08:51:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const linkDiv = document.getElementById("divLink")
 
-linkDiv.addEventListener((e) => {
+linkDiv.addEventListener("click", () => {
 
 window.open("https://www.figma.com/proto/VGIeXCOyGLEANv5VcUZvGO/Portfolio-UX?page-id=0%3A1&node-id=98-264&viewport=-553%2C933%2C0.19&t=lxrL55vhUaQDs5h")
   
